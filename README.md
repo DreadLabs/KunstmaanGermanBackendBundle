a70f7e6911be0718e1c5324fb9e046a4ce3a7263
Tommy Juhnke <dev@van-tomas.de>
2016-09-21 19:34:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kunstmaan German Backend Bundle
+
+## What does it do?
+
+Adds and overrides german translations for the Kunstmaan CMS backend.
+
+## Installation
+
+    composer install dreadlabs/kunstmaan-german-backend-bundle
+
+## How to use?
+
+  1. Load the bundle in your `AppKernel`
+  
+         // snip
+         new DreadLabs\KunstmaanGermanBackendBundle\DreadLabsKunstmaanGermanBackendBundle()
+         // snap
---
+++ src/DreadLabsKunstmaanGermanBackendBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the `DreadLabs/KunstmaanGermanBackendBundle` project.
+ *
+ * (c) https://github.com/DreadLabs/KunstmaanGermanBackendBundle/graphs/contributors
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace DreadLabs\KunstmaanGermanBackendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DreadLabsKunstmaanGermanBackendBundle extends Bundle
+{
+}
